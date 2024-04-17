c341e2476bcb09c5d02f829061e583898bcd8034
Mark-WebHabit <markbobis173@gmail.com>
2024-04-17 22:50:18 +0800

    debug3 fixed forgotpass

--- server.js
+++ server.js
@@ -68,8 +68,6 @@ app.use("/admin", adminRoutes);
 // custom middleware to catch errors
 app.use(errorLogger);
 
-console.log("hello world");
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
